db4aca3b8c5b56fe298d5a8a6e37414822438ef1
Striz <63916202+Striz-pixel@users.noreply.github.com>
2022-04-03 18:38:42 +0500

    Add files via upload

---
+++ db.js
@@ -0,0 +1,32 @@
+let es = {
+    "title": "Выбор персонажа Dota 2",
+    "start": "Какую позицию вы выберите ",
+    "dict": {
+        "Какую позицию вы выберите ": ["Mid line","Safe line","Hard line","Soft support","Hard support"],
+        "Mid line":["Выберите легкого персонажа","Выберите среднего пресонажа ","Выберите тяжелого персонажа"],
+        "Safe line":["Выберите Легкого персонажа","Выберите Среднего пресонажа ","Выберите Тяжелого персонажа"],
+        "Hard line":["Выберите Легкого Персонажа","Выберите Среднего Пресонажа ","Выберите Тяжелого Персонажа"],
+        "Soft support":["Выбрать легкого персонажа","Выбрать среднего пресонажа ","Выбрать тяжелого персонажа"],
+        "Hard support":["Выбрать Легкого персонажа","Выбрать Среднего пресонажа ","Выбрать Тяжелого персонажа"],
+
+        "Выберите легкого персонажа":["Tinker","Pudge","Sniper"],
+        "Выберите среднего пресонажа ":["TemplarAssassin","ShadowFind","StormSpirit"],
+        "Выберите тяжелого персонажа":["Invoker","BroodMother","ArcWarden"],
+
+        "Выберите Легкого персонажа":["WraitKing","ChaosKnight","PhantomAssassin"],
+        "Выберите Среднего пресонажа ":["PhantomLancer","Antimage","FacelessVoid"],
+        "Выберите Тяжелого персонажа":["LounDruide","ArcWarden","Morphling"],
+
+        "Выберите Легкого Персонажа":["Axe","Viper","Timbersow"],
+        "Выберите Среднего Пресонажа ":["SandKing","Legioncommander","Venomancer"],
+        "Выберите Тяжелого Персонажа":["Darkseer","Necrophos"],
+
+        "Выбрать легкого персонажа":["Techis","Pudge","Riki"],
+        "Выбрать среднего пресонажа ":["SkyWrath","Rubick","SpiritBreaker"],
+        "Выбрать тяжелого персонажа":["NatureProphet","Luna","BountyHunter"],
+
+        "Выбрать Легкого персонажа":["Dazzle","Ogremagi","KeeperOfTheLight"],
+        "Выбрать Среднего пресонажа ":["Lion","Undying","Jakiro"],
+        "Выбрать Тяжелого персонажа":["Oracle","WinterWyvern","Visage"],
+    }
+}
